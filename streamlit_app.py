28847cf01bdfe1d72c0e860bd88d93858d2dbbd7
aswanigaddam <168556199+aswanigaddam@users.noreply.github.com>
2024-05-10 16:14:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrient Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         #st.text(fruityvice_response)
         #st.text(fruityvice_response.json())
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
